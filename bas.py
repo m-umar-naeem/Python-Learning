bb5709fb8fcff88d1a5553809b18e7158e96d04c
m-umar-naeem <un963290@gmail.com>
2025-10-25 17:47:59 +0500

    Add files via upload

---
+++ bas.py
@@ -0,0 +1,17 @@
+# a='p'
+# b='c'
+# print(a + "-" + b)
+# x='I AM GOOD'
+# print(x[2])
+# print(x[-2])
+# print(x[2:6])
+# x.lower()
+# print(x.lower(5))
+# x=input()
+# if x>5:
+#     print("x+1")
+# else:
+#     print(x)
+a='ok'
+# a[0]='i'
+print(a[0])
